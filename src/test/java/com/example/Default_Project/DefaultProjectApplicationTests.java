c2611e9bab40afe3193cdbff1dd8d60d1b3a4444
OTIS <72382663+1domybest@users.noreply.github.com>
2025-01-26 13:33:22 +0900

    init
    init

---
+++ src/test/java/com/example/Default_Project/DefaultProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Default_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
