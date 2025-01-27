0dc0aa087e1e9173b6c132704f226a8748d40a5a
OTIS <72382663+1domybest@users.noreply.github.com>
2025-01-27 22:45:07 +0900

    .
    .

---
+++ src/main/java/com/example/Default_Project/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.Default_Project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/tokenTest")
+    public String test(){
+        return "test";
+    }
+}
