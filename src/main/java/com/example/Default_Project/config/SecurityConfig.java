a92e665542f20d6e38bf22d08f4b31d2de2f5166
OTIS <72382663+1domybest@users.noreply.github.com>
2025-02-17 19:26:33 +0900

    a

--- src/main/java/com/example/Default_Project/config/SecurityConfig.java
+++ src/main/java/com/example/Default_Project/config/SecurityConfig.java
@@ -137,7 +137,7 @@ public class SecurityConfig {
 
 
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/hc", "/env").permitAll() // 무중단 배포용
+                .requestMatchers("/api/hc", "/api/env").permitAll() // 무중단 배포용
                 .requestMatchers("/login", "/join").permitAll() // 허용
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .requestMatchers("/token-refresh").permitAll()
