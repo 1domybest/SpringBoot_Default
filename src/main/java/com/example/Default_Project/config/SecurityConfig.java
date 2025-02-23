3bdf79157c207292609b902b3a9ff8d1be959940
OTIS <72382663+1domybest@users.noreply.github.com>
2025-02-23 21:12:29 +0900

    g

--- src/main/java/com/example/Default_Project/config/SecurityConfig.java
+++ src/main/java/com/example/Default_Project/config/SecurityConfig.java
@@ -139,26 +139,26 @@ public class SecurityConfig {
 
         //oauth2
         // 소셜로그인시 아래 필터에 걸림
-//        http
-//                .oauth2Login((oauth2) -> oauth2
-//                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-//                                .userService(customOAuth2UserService))
-//                        .successHandler(customSuccessHandler)
-//                );
-
         http
-                .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(auth -> auth
-                                .baseUri("/oauth2/authorize") // 인증 요청 URL 변경
-                        )
-                        .redirectionEndpoint(redirection -> redirection
-                                .baseUri("/oauth2/code/*") // 리디렉션 URI 변경
-                        )
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(customOAuth2UserService) // 사용자 정보 가져오기
-                        )
-                        .successHandler(customSuccessHandler) // 로그인 성공 핸들러
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
                 );
+
+//        http
+//                .oauth2Login(oauth2 -> oauth2
+//                        .authorizationEndpoint(auth -> auth
+//                                .baseUri("/oauth2/authorize") // 인증 요청 URL 변경
+//                        )
+//                        .redirectionEndpoint(redirection -> redirection
+//                                .baseUri("/oauth2/code/*") // 리디렉션 URI 변경
+//                        )
+//                        .userInfoEndpoint(userInfo -> userInfo
+//                                .userService(customOAuth2UserService) // 사용자 정보 가져오기
+//                        )
+//                        .successHandler(customSuccessHandler) // 로그인 성공 핸들러
+//                );
 
         /*
          * before At after 을 사용하는 이유는 이걸 지정하지않고 At을 사용한다면
